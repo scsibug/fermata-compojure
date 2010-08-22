33d84280c95cbeed99773acbf27670443915cc9c
Greg Heartsfield <scsibug@imap.cc>
2010-08-21 23:46:06 -0500

    Added dependency for SubEthaSMTP as a mailserver.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
                  [org.clojure/clojure-contrib "1.2.0-RC3"]
                  [compojure "0.4.0"]
-                 [ring/ring-jetty-adapter "0.2.3"]]
+                 [ring/ring-jetty-adapter "0.2.3"]
+                 [org.subethamail/subethasmtp "3.1.4"]]
   :main fermata.core)
