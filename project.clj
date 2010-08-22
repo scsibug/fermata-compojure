a33da78f8c1712ebb954e7260fbd3fc751c3538d
Greg Heartsfield <scsibug@imap.cc>
2010-08-21 23:16:32 -0500

    Compojure hello-world setup.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject fermata "0.1.0-SNAPSHOT"
   :description "Fake MTA for application testing"
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]])
+                 [org.clojure/clojure-contrib "1.2.0-RC3"]
+                 [compojure "0.4.0"]
+                 [ring/ring-jetty-adapter "0.2.3"]]
+  :main fermata.core)
--- src/fermata/core.clj
+++ src/fermata/core.clj
@@ -1,1 +1,12 @@
-(ns fermata.core)
+(ns fermata.core
+  (:use compojure.core
+        ring.adapter.jetty)
+  (:require [compojure.route :as route])
+  (:gen-class))
+
+(defroutes example
+  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (route/not-found "Page not found"))
+
+(defn -main [& args]
+  (run-jetty example {:port 8080}))
