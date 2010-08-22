dcc74b2a18e44b7f12b3dc41abb059bea9e764c4
Greg Heartsfield <scsibug@imap.cc>
2010-08-21 23:03:23 -0500

    Initial commit after initializing directory with lein.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject fermata "0.1.0-SNAPSHOT"
+  :description "Fake MTA for application testing"
+  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
+                 [org.clojure/clojure-contrib "1.2.0-RC3"]])
---
+++ test/fermata/test/core.clj
@@ -0,0 +1,6 @@
+(ns fermata.test.core
+  (:use [fermata.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
